19d3ed3b47f6fe388b61fd0b397a1031a5625250
YuyingHUANG <44936202+YuyingHUANG@users.noreply.github.com>
2020-01-26 13:22:10 -0800

    Add files via upload

--- R/list.learners.R
+++ R/list.learners.R
@@ -2,6 +2,7 @@ list.learners <- function(){
 
   learners <- c("lm1", "glm1", "glmnet1", "glmnet.lasso", "glmnet.ridge", "rpart1",
                 "lda1", "qda1", "KNN1", "svm1")
+  print("Models that can be chosen are:")
   print(learners)
 
 }
